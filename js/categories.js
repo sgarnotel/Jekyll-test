4ef681b210eb2055b1bbc8ca1c87ada998db9c24
Simon Garnotel <simon.garnotel@gmail.com>
2018-10-26 11:05:54 +0200

    dynamical categories

--- js/categories.js
+++ js/categories.js
@@ -1,5 +1,20 @@
 categories = []
 
 addToCategories = (item) => {
+  addCategory(item)
   categories.push(item)
+}
+
+addCategory = (item) => {
+  const label = document.createElement('label')
+  label.className = 'light light-' + item
+  label.innerHTML = item
+  const input = document.createElement('input')
+  input.type = 'checkbox'
+  input.checked = true
+  input.id = item
+  input.onChange = function() { onCategoryChange(item, input.checked) }
+  label.appendChild(input)
+  
+  categoriesDiv.appendChild(label)
 }
